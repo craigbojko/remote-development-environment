38bb8fc222a74bf390b4b294ba467cd4786d7b5c
Craig Bojko <craig@pixelventures.co.uk>
2025-09-24 19:55:18 +0100

    feat: move to environment directories
    - each device class has it's own directory and playbook
    - better defaults and variable insertion
    - makefile to consolidate cli arguments
    - run.sh updated to handle cli arguments
    - added initial k3s node class

---
+++ makefile
@@ -0,0 +1,24 @@
+.PHONY: run
+
+install:
+	ansible-galaxy install -r requirements.yaml
+	ansible-galaxy collection install -r requirements.yaml
+
+.run:
+	if [ -z "$(environment)" ]; then echo "Please specify an environment, e.g., make dev-server"; exit 1; fi
+	./run.sh $(environment)
+
+dev-server:
+	./run.sh dev-server \
+		--hostname=$(hostname) \
+		--remote=$(user)
+
+zigbee-station:
+	./run.sh zigbee-station \
+		--hostname=$(hostname) \
+		--remote=$(user)
+
+k3s-node:
+	./run.sh k3s-node \
+		--hostname=$(hostname) \
+		--remote=$(user)
--- run.sh
+++ run.sh
@@ -6,19 +6,62 @@
 # Created Date: Saturday, January 4th 2025, 4:43:07 pm
 # Author: Craig Bojko
 # -----
-# Last Modified: Tue Jul 15 2025
+# Last Modified: Wed Sep 24 2025
 # Modified By: Craig Bojko
 # -----
 # Copyright (c) 2025 Pixel Ventures Ltd.
 # ------------------------------------
 # <<licensetext>>
 
-# Run ansible playbook with all vars files
+#!/usr/bin/env bash
+
+environment=$1
+shift  # Remove the first argument (environment)
+
+if [ -z "$environment" ]; then
+  echo "Usage: $0 <environment> [--key=value...]"
+  echo "Example: $0 dev-server --hostname=my-host --port=9999"
+  exit 1
+fi
+
+if [ ! -f "environments/$environment/playbook.yaml" ]; then
+  echo "Environment '$environment' does not exist."
+  exit 1
+fi
+
+echo "Arguments: $@"
+
+# Build extra-vars from remaining arguments
+extra_vars=""
+for arg in "$@"; do
+  # Check if argument starts with --
+  if [[ $arg == --* ]]; then
+    # Remove the -- prefix and add to extra vars
+    key_value="${arg#--}"
+    key="${key_value%%=*}"
+    value="${key_value#*=}"
+
+    # Skip if key or value is empty
+    if [ -z "$key" ] || [ -z "$value" ]; then
+      continue
+    fi
+
+    extra_vars="$extra_vars --extra-vars $key=$value"
+  fi
+done
+
+echo "Running playbook for environment: $environment"
+echo "With extra vars: $extra_vars"
+
+# Set the script directory as working directory and config path
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "Script directory: $SCRIPT_DIR"
+cd "$SCRIPT_DIR"
+export ANSIBLE_CONFIG="$SCRIPT_DIR/ansible.cfg"
 
 ansible-playbook \
   -i hosts \
-  --extra-vars "@vars/remote-user.yaml" \
-  --extra-vars "@vars/resilio.yaml" \
-  --extra-vars "@vars/oh-my-zsh.yaml" \
+  --extra-vars '@vars/defaults.yaml' \
+  $extra_vars \
   --verbose \
-  playbook.yaml
+  environments/$environment/playbook.yaml
